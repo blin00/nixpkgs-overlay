09eb0325d82228dd856482b2ffc3a93b1a468455
Brandon Lin <blin00@gmail.com>
2020-11-23 04:09:14 -0800

    update fr24feed

--- fr24feed/default.nix
+++ fr24feed/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, radare2 }: (
 
-assert stdenv.lib.asserts.assertMsg (stdenv.hostPlatform.system == "aarch64-linux") "fr24feed is only supported on aarch64-linux";
+assert stdenv.lib.asserts.assertMsg (stdenv.hostPlatform.isAarch32 || stdenv.hostPlatform.isAarch64) "fr24feed is only supported on ARM";
 
 stdenv.mkDerivation rec {
   pname = "fr24feed";
-  version = "1.0.24-4";
+  version = "1.0.26-9";
 
   src = fetchurl {
     url = "https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_${version}_armhf.tgz";
-    sha256 = "12m9272sagg534mdn1ilydyycs8hz7srhcnznl51w7g52sqmp55p";
+    sha256 = "09a2cpsmac76bnf7p5mb0wd5vwvv6zr2jwxw1d8dapmypxr2p9hm";
   };
 
   nativeBuildInputs = [ radare2 ];
@@ -17,7 +17,7 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/bin
     cp fr24feed $out/bin
-    ${radare2}/bin/r2 -Q -w -c '/ /bin/bash; s hit0_0; wz /bin/sh; / hostname -I; s hit1_0; wz hostname -i; / /sbin/rmmod; s hit2_0; wx 2300; wa nop @ 0x000fc7e4; wa ldr r1, [r3] @ 0x000fc7e8; wa ldr r2, [r3, 4] @ 0x000fc7ec' $out/bin/fr24feed
+    ${radare2}/bin/r2 -Q -w -c '/ /bin/bash; s hit0_0; wz /bin/sh; / /sbin/rmmod; s hit1_0; wx 2300' $out/bin/fr24feed
   '';
 
   meta = {

4e6f9c9b8ac2aed9e57e91607eda8fa2cac7428a
Brandon Lin <blin00@gmail.com>
2022-10-09 10:14:35 +0000

    update fr24feed to 1.0.30-3

--- fr24feed/default.nix
+++ fr24feed/default.nix
@@ -4,11 +4,11 @@ assert lib.asserts.assertMsg (stdenv.hostPlatform.isAarch32 || stdenv.hostPlatfo
 
 stdenv.mkDerivation rec {
   pname = "fr24feed";
-  version = "1.0.29-8";
+  version = "1.0.30-3";
 
   src = fetchurl {
     url = "https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_${version}_armhf.tgz";
-    sha256 = "1sxg30szzfg68vcnd67kk64sl1xyr2dyzj67bmsafv27ck6jzibj";
+    sha256 = "1z8g8hm580zf3lf8mc7nkzxh2c4bw3qq2padymwzs1ckidg7x344";
   };
 
   nativeBuildInputs = [ radare2 ];

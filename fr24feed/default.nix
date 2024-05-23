39479d3aeffc9a3c73d45be253299f4f82b0a845
Brandon Lin <blin00@gmail.com>
2024-05-23 06:46:47 +0000

    fr24feed 1.0.34-0 => 1.0.48-0 and switch to arm64 build

--- fr24feed/default.nix
+++ fr24feed/default.nix
@@ -1,14 +1,14 @@
 { lib, stdenv, fetchurl, radare2 }: (
 
-assert lib.asserts.assertMsg (stdenv.hostPlatform.isAarch32 || stdenv.hostPlatform.isAarch64) "fr24feed is only supported on ARM";
+assert lib.asserts.assertMsg (stdenv.hostPlatform.isAarch64) "fr24feed is only supported on arm64";
 
 stdenv.mkDerivation rec {
   pname = "fr24feed";
-  version = "1.0.34-0";
+  version = "1.0.48-0";
 
   src = fetchurl {
-    url = "https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_${version}_armhf.tgz";
-    sha256 = "1ikj2bwa8zbca6qrlsmi29mpmwkblz1wfhsgay87w1ql22169k9c";
+    url = "https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_${version}_arm64.tgz";
+    sha256 = "09pvck6npk78l46l59d5p92ic1gcp8iqlvhddha3f2ijwn8psnkd";
   };
 
   nativeBuildInputs = [ radare2 ];

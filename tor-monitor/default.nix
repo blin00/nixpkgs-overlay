2c051d60a397bcf9ad9e83c2235ee53f14339ca0
Brandon Lin <blin00@gmail.com>
2025-01-23 02:46:32 -0800

    pin stem version

--- tor-monitor/default.nix
+++ tor-monitor/default.nix
@@ -1,14 +1,38 @@
-{ pkgs, stdenv }: (
+{ pkgs, stdenv, fetchFromGitHub }: (
 
+let
+  python = let
+    packageOverrides = self: super: {
+      stem = super.stem.overridePythonAttrs(old: rec {
+        version = "1.8.2";
+        src = fetchFromGitHub {
+          owner = "torproject";
+          repo = "stem";
+          rev = "refs/tags/${version}";
+          hash = "sha256-9BXeE/sVa13jr8G060aWjc49zgDVBhjaR6nt4lSxc0g=";
+        };
+        patches = [];
+        checkPhase = ''
+          runHook preCheck
+          runHook postCheck
+        '';
+      });
+    };
+  in pkgs.python312.override {inherit packageOverrides; self = python;};
+in
 stdenv.mkDerivation rec {
   name = "tor-monitor";
   propagatedBuildInputs = [
-    (pkgs.python3.withPackages (ps: with ps; [
+    (python.withPackages (ps: with ps; [
       stem
     ]))
   ];
   dontUnpack = true;
-  installPhase = "install -Dm755 ${./tor-monitor.py} $out/bin/tor-monitor";
+  installPhase = ''
+    runHook preInstall
+    install -Dm755 ${./tor-monitor.py} $out/bin/tor-monitor
+    runHook postInstall
+  '';
 }
 
 )

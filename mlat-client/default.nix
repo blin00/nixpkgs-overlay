c3be0f9fe4b398aff784f3432a4e704a58c7d242
Brandon Lin <blin00@gmail.com>
2025-12-06 23:45:16 -0800

    mlat-client: fix build for nixos 25.11

--- mlat-client/default.nix
+++ mlat-client/default.nix
@@ -10,6 +10,8 @@ python311.pkgs.buildPythonPackage rec {
     rev = "fabefdc3543702c6ef950111b94981162c45147d";
     sha256 = "1s336ls0sll49bqcidwx9dprby11dvvkpxb64l66m11wknhk4n7c";
   };
+  pyproject = true;
+  build-system = [ python311.pkgs.setuptools ];
 }
 
 )

63bfc2571bc968e6fbf77d50e8d7ddfe1dcac087
Brandon Lin <blin00@gmail.com>
2025-12-26 02:32:16 -0800

    piaware: patch a bunch of absolute paths

--- piaware/default.nix
+++ piaware/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, openssl, tcl, tcllauncher, tclPackages }:
+{ stdenv, fetchFromGitHub, dump1090-fa, fa-mlat-client, openssl, tcl, tcllauncher, tclPackages }:
 
 stdenv.mkDerivation rec {
   pname = "piaware";
@@ -12,13 +12,17 @@ stdenv.mkDerivation rec {
   };
 
   nativeBuildInputs = [ openssl tcl ];
-  buildInputs = [ tcllauncher tcl.tclPackageHook tclPackages.tcllib tclPackages.tcltls tclPackages.tclx ];
-  phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
+  buildInputs = [ dump1090-fa fa-mlat-client tcllauncher tcl.tclPackageHook tclPackages.tcllib tclPackages.tcltls tclPackages.tclx ];
+  phases = [ "unpackPhase" "patchPhase" "installPhase" "fixupPhase" ];
+
+  patches = [ ./helpers-path.patch ];
 
   installPhase = ''
     runHook preInstall
     export PATH="$PATH''${PATH:+:}${openssl}/bin"
     make DESTDIR="$out" PREFIX= TCLLAUNCHER="${tcllauncher}/bin/tcllauncher" TCLSH="${tcl}/bin/tclsh" install
+    ln -s "${dump1090-fa}/bin/faup1090" "$out/lib/piaware/helpers/faup1090"
+    ln -s "${fa-mlat-client}/bin/fa-mlat-client" "$out/lib/piaware/helpers/fa-mlat-client"
     runHook postInstall
   '';
 }

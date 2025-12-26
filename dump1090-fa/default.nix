cb2da1b5a3eb35357f8634be3516640daf5ab39b
Brandon Lin <blin00@gmail.com>
2025-12-26 02:31:58 -0800

    dump1090-fa: also build faup1090

--- dump1090-fa/default.nix
+++ dump1090-fa/default.nix
@@ -16,10 +16,14 @@ stdenv.mkDerivation rec {
   # omit stuff we don't use
   buildInputs = [ libusb1 ncurses rtl-sdr ];
 
+  postBuild = ''
+    make faup1090
+  '';
+
   installPhase = ''
     runHook preInstall
     mkdir -p $out/bin $out/share
-    cp dump1090 view1090 $out/bin
+    cp dump1090 faup1090 view1090 $out/bin
     cp -r public_html $out/share/dump1090
     runHook postInstall
   '';

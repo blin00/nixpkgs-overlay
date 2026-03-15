e00586a6743e4ad8876e605669f879c802dde8dc
Brandon Lin <blin00@gmail.com>
2026-03-15 16:54:28 -0700

    fr24feed 1.0.51-0 => 1.0.54-0

--- fr24feed/default.nix
+++ fr24feed/default.nix
@@ -4,11 +4,11 @@ assert lib.asserts.assertMsg (stdenv.hostPlatform.isAarch64) "fr24feed is only s
 
 stdenv.mkDerivation rec {
   pname = "fr24feed";
-  version = "1.0.51-0";
+  version = "1.0.54-0";
 
   src = fetchurl {
     url = "https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_${version}_arm64.tgz";
-    sha256 = "0cmjl38bf3pg99mgqng2w3505dsm92vs9zhz02y39w05b6fqi8dq";
+    hash = "sha256-se7WgYa8U5SOhYvUJ5giREL9INNq/U5nRegSKDBNL6s=";
   };
 
   nativeBuildInputs = [ radare2 ];
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
     runHook preInstall
     mkdir -p $out/bin
     cp fr24feed $out/bin
-    ${radare2}/bin/r2 -Q -w -c '/ /bin/bash; s hit0_0; wz /bin/sh; / /sbin/rmmod; s hit1_0; wz "#"; / /usr/bin/pgrep; s hit2_0; wz pgrep; s hit2_1; w ";;;;;;;;;pgrep"; / /bin/netstat; s hit3_0; w ";;;;;"' $out/bin/fr24feed
+    ${radare2}/bin/r2 -Q -w -c '/ /bin/bash; s hit0_0; wz /bin/sh; / /sbin/rmmod; s hit1_0; wz "#"; / /usr/bin/pgrep; s hit2_0; wz pgrep; s hit2_1; wz false; / /bin/netstat; s hit3_0; w ";;;;;"' $out/bin/fr24feed
     runHook postInstall
   '';
 

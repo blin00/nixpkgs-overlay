cfcff3468804db67b12a8c8fa9ce54f459c274b9
Brandon Lin <blin00@gmail.com>
2026-06-01 01:23:59 -0700

    dump1090-fa: add werror patch back

--- dump1090-fa/default.nix
+++ dump1090-fa/default.nix
@@ -11,6 +11,8 @@ stdenv.mkDerivation rec {
     hash = "sha256-kTJ8FMugBRJaxWas/jEj4E5TmVnNpNdhq4r2YFFwgTU=";
   };
 
+  patches = [ ./werror.patch ];
+
   nativeBuildInputs = [ pkg-config ];
 
   # omit stuff we don't use

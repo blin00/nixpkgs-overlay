6b7f9c42af2e5f22dd2a5264e7b569ef71b3848a
Brandon Lin <blin00@gmail.com>
2026-06-01 01:38:21 -0700

    mlat-client: fix broken hash

--- mlat-client/default.nix
+++ mlat-client/default.nix
@@ -8,7 +8,7 @@ python311.pkgs.buildPythonApplication rec {
     owner = "adsbxchange";
     repo = "mlat-client";
     rev = "v${version}";
-    sha256 = "1s336ls0sll49bqcidwx9dprby11dvvkpxb64l66m11wknhk4n7c";
+    hash = "sha256-V//LpYmBXtT8haX1aZ4XldzzyUY2YN7x3lTpQ2csTmw=";
   };
   pyproject = true;
   build-system = [ python311.pkgs.setuptools ];
